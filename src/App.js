492662c1a755e020fce32ff37150ccb90db74354
Roman Moskalenko <skalermo@gmail.com>
2020-12-15 16:57:02 +0100

    Separate header

--- src/App.js
+++ src/App.js
@@ -5,27 +5,13 @@ import { Link, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Teachers from "./components/Teachers";
 import Activities from "./components/Activities";
-// import Category from "./components/Category";
 
-// const Home = () => (
-//   <div>
-//     <h2>Home</h2>
-//     <p>Home screen for School Scheduler</p>
-//   </div>
-// );
+const Header = () => {
+  return (
+    <header>
+      <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+        <div class="container">
 
-const Products = () => (
-  <div>
-    <h2>Products</h2>
-  </div>
-);
-
-class App extends Component {
-  render() {
-
-    return (
-      <div class="col-xs-1" align="center" >
-        < nav class="navbar navbar-expand-lg navbar-light bg-light" >
           <a class="navbar-brand" href="#">School Scheduler</a>
           <ul className="nav navbar-nav">
             <li>
@@ -38,16 +24,31 @@ class App extends Component {
               <Link class="nav-link" to="/activities">Activities</Link>
             </li>
           </ul>
-        </nav >
+        </div>
+      </nav >
+    </header>
+  )
+}
 
-        <Switch>
-          <Route exact path="/"><Home /></Route>
-          <Route path="/teachers"><Teachers /></Route>
-          <Route path="/activities"><Activities /></Route>
-          <Route path="/:id">
-            <p>This text will render for any route other than those defined above</p>
-          </Route>
-        </Switch>
+class App extends Component {
+  render() {
+
+    return (
+      <div>
+        <Header />
+
+        <div class="container">
+          <main role="main" class="pb-3">
+            <Switch>
+              <Route exact path="/"><Home /></Route>
+              <Route path="/teachers"><Teachers /></Route>
+              <Route path="/activities"><Activities /></Route>
+              <Route path="/:id">
+                <p>This text will render for any route other than those defined above</p>
+              </Route>
+            </Switch>
+          </main>
+        </div>
 
       </div >
     );
