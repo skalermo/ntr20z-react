bee9551550f4fc2f03341f8937b5ad342e6ae141
Roman Moskalenko <skalermo@gmail.com>
2020-12-21 14:03:33 +0100

    Handle activity displaying, refactor code

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,83 +1,117 @@
-import React, { Component } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { Link } from "react-router-dom";
 import "../grid.css"
-// import { Link, Route, useParams, useRouteMatch } from "react-router-dom"
 
 const headerSlots = ["time", "Mo", "Tu", "We", "Th", "Fr"];
 const activitiesTimes = [
     "8:00-8:45", "8:55-9:40", "9:50-11:35",
     "11:55-12:40", "12:50-13:55", "13:45-14:30",
     "14:40-15:25", "15:35-16:20", "16:30-17:15"];
-const activityLabels = [];
 const rows = 9;
 const cols = 5;
 
-function Temp() {
-    return (
-        <div class="wrapper">
-            {headerSlots.map((slot) => {
-                return (
-                    <div class="slot header">{slot}</div>
-                )
-            })}
+function Activities() {
+    const [selectedGroupIdx, setSelectedGroupIdx] = useState();
+    const [groups, setGroups] = useState([]);
+    const [labels, setLabels] = useState([]);
+    const firstUpdate = useRef(true);
 
-            {Array.apply(null, Array(cols * rows)).map(function (x, i) {
-                console.log(i);
-                let f = () => {
-                    if (i % cols == 0)
-                        return (
-                            <>
-                                <div class="slot index">{activitiesTimes[i / cols]}</div>
-                                <a href="" class="fill-div slot" data-toggle="modal" data-target="#activityModal">
-                                    a
-                                </a>
-                            </>
-                        )
-                    return (
-                        <a href="" class="fill-div slot" data-toggle="modal" data-target="#activityModal">
-                            a
-                        </a>
-                    )
-                }
-                return f();
-            })}
-        </div >
-    )
-}
-
-class Activities extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            activities: [],
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <h2>
-                    Activities
-                </h2 >
-
-                <Temp />
-
-            </div>
-        );
-    }
-
-    fetchActivities() {
-        fetch("http://localhost:8081/activities")
+    const fetchGroups = () => {
+        fetch("http://localhost:8081/groups")
             .then(res => res.json())
             .then((data) => {
-                this.setState({ activities: data })
+                if (groups.length === 0) {
+                    setGroups(data);
+                    setSelectedGroupIdx(0);
+                }
+                setGroups(data);
             })
-            .catch(console.log)
+            .catch(console.log);
     }
 
-    componentDidMount() {
-        this.fetchActivities();
-        this.timer = setInterval(() => this.fetchActivities(), 10_000);
+    const fetchActivities = (selectedGroupIdx) => {
+        fetch(`http://localhost:8081/groups/${selectedGroupIdx}/activities`)
+            .then(res => res.json())
+            .then((data) => {
+                let labels = Array.apply([-1, ""], Array(rows * cols));
+                data.forEach(function (item) {
+                    labels[item.slot] = [item.slot, [item.room, item.subject].join(" ")];
+                })
+                setLabels(labels);
+            })
+            .catch(console.log);
     }
+
+    useEffect(() => {
+        fetchGroups();
+    }, [])
+    useEffect(() => {
+        if (firstUpdate.current) {
+            firstUpdate.current = false;
+        } else {
+            fetchActivities(selectedGroupIdx);
+        }
+    }, [selectedGroupIdx]);
+
+    return (
+        <div>
+            <h2>
+                Activities
+                </h2 >
+            <div class="modal-body">
+                <div class="form-group row">
+                    <label class="col-form-label">Group</label>
+                    <div class="col-sm-3">
+                        <select class="form-control" onChange={e => setSelectedGroupIdx(e.target.value)}>
+                            {/* <option value="" disabled selected>Select a group</option> */}
+                            {groups && groups.length && groups.map((g, idx) => {
+                                return <option value={idx}>{g}</option>
+                            })}
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            {(() => {
+                if (selectedGroupIdx === undefined)
+                    return (
+                        <p>Select group to load schedule table</p>
+                    )
+                else {
+                    return (
+                        <div class="wrapper">
+                            {headerSlots.map((slot) => {
+                                return (
+                                    <div class="slot header">{slot}</div>
+                                )
+                            })}
+
+                            {labels.map(function (x, i) {
+                                // console.log(i);
+                                return (() => {
+                                    if (i % cols == 0)
+                                        return (
+                                            <>
+                                                {/* <Link class="fill-div slot" to={{
+                                        pathname: "/activityForm",
+                                        state: { idx }
+                                    }}>Edit</Link> */}
+
+                                                <div class="slot index">{activitiesTimes[i / cols]}</div>
+                                                <Link class="fill-div slot" to="/activityForm">{x}</Link>
+                                            </>
+                                        )
+                                    return (
+                                        <Link class="fill-div slot" to="/activityForm">{x}</Link>
+                                    )
+                                })();
+                            })}
+                        </div>
+                    )
+                }
+            })()}
+        </div >
+    )
 }
 
 export default Activities;
